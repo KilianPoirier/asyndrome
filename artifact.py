24af6031fd994b92bb4cde99c46392a8bb818bc9
acasta-yhliu <yhliu2000@outlook.com>
2026-01-18 12:36:07 -0500

    [fix] fixed naming issue

--- artifact.py
+++ artifact.py
@@ -18,34 +18,34 @@ def table2():
         elif case in ("hypersurface", "xzzx", "defect"):
             decoders = "pymatching"
 
-        os.system(f"python3 experiment-general.py {case} -d {decoders}")
+        os.system(f"python3 evaluate-general.py {case} -d {decoders}")
 
 
 def table3():
     print("Expect duration: ~5mins")
-    os.system("python3 experiment-crossdec.py")
+    os.system("python3 evaluate-crossdec.py")
 
 
 def figure12():
     print("Expect duration: ~2mins")
-    os.system("python3 experiment-surface.py")
+    os.system("python3 evaluate-surface.py")
 
 
 def figure13():
     print("Expect duration: ~2mins")
-    os.system("python3 experiment-bbcode.py")
+    os.system("python3 evaluate-bbcode.py")
 
 
 def figure14():
     print("Expect duration: ~2mins")
     for code in ("color-hex-3", "hypercolor-24-8-4-4", "hypersurface-30-8-3-3"):
-        os.system(f"python3 experiment-scaling.py evaluate {code}")
+        os.system(f"python3 evaluate-scaling.py evaluate {code}")
 
 
 def figure15():
     print("Expect duration: ~15mins")
     for action in ("evaluate", "plot"):
-        os.system(f"python3 experiment-nonuniform.py {action}")
+        os.system(f"python3 evaluate-nonuniform.py {action}")
 
 
 if len(sys.argv) != 2:
@@ -63,14 +63,5 @@ if asset not in ("table2", "table3", "figure12", "figure13", "figure14", "figure
     print("The asset you want to reproduce does not exist.")
     exit(-1)
 
-print(
-    f"Reproducing asset: {asset}. The result will be output to the folder 'asset' with the same name."
-)
-
-if os.path.exists(f"./asset/{asset}"):
-    print("Previous asset exists, deleting.")
-    shutil.rmtree(f"./asset/{asset}")
-
-os.makedirs(f"./asset/{asset}", exist_ok=True)
 
 globals()[asset]()
